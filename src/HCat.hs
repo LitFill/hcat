4b8260a50c98cc847b05cd19c225cc9a3236b598
LitFill <marrazzy54@gmail.com>
2024-12-16 18:12:14 +0700

    feat(lib): add functionality for pagination
    this includes:
      - getting terminal dimensions
      - paginate the content of the file
      - wrapping the lines of the content based on terminal sizes

--- src/HCat.hs
+++ src/HCat.hs
@@ -1,26 +1,127 @@
 module HCat where
 
-import Control.Exception qualified as Exception
-import Data.Text.IO qualified as TextIO
-import System.Environment qualified as Env
-import System.IO.Error qualified as IOError
-
 import Flow
 
-data Result a = Err String | Ok a
+import Control.Exception qualified as Exception
+import Data.Text (Text)
+import Data.Text qualified as Text
+import Data.Text.IO qualified as TextIO
+import Data.Tuple.Extra qualified as Tuple
+import System.Environment qualified as Env
+import System.IO.Error qualified as IOError
+import System.Info qualified as SysInfo
+import System.Process qualified as Proc
+
+data Result a where
+    Err :: String -> Result a
+    Ok :: a -> Result a
 
 toIOError :: Result a -> IO a
 toIOError (Ok a) = return a
 toIOError (Err e) =
     IOError.userError e |> Exception.throwIO
 
+data ScreenDimensions where
+    ScreenDimensions
+        :: {screenRows :: Int, screenCols :: Int}
+        -> ScreenDimensions
+    deriving (Show)
+
+data CancelContinue where
+    Cancel :: CancelContinue
+    Continue :: CancelContinue
+    deriving (Show)
+
 handleArgs
     :: [String] -> Result FilePath
 handleArgs [filepath] = Ok filepath
-handleArgs [] = Err "ERROR: no arguments provided!"
+handleArgs [] =
+    Err "ERROR: no arguments provided!"
 handleArgs _ =
     Err
         "ERROR: too much arguments provided!"
+
+groupsOf :: Int -> [a] -> [[a]]
+groupsOf _ [] = []
+groupsOf 0 lst = [lst]
+groupsOf n lst =
+    let (x, xs) = splitAt n lst
+     in x : groupsOf n xs
+
+groupsOf' :: Int -> [a] -> [[a]]
+groupsOf' n =
+    splitAt n
+        .> Tuple.second (groupsOf n)
+        .> uncurry (:)
+
+wordWrap :: Int -> Text -> [Text]
+wordWrap maxColumn text
+    | Text.length text <= maxColumn = [text]
+    | otherwise =
+        let
+            (candidate, nextLines) = Text.splitAt maxColumn text
+            (firstLine, overflow) =
+                softWrap
+                    candidate
+                    (Text.length candidate - 1)
+         in
+            firstLine
+                : wordWrap
+                    maxColumn
+                    (overflow <> nextLines)
+  where
+    softWrap hardwrappedText textIdx
+        | textIdx <= 0 =
+            (hardwrappedText, Text.empty)
+        | Text.index hardwrappedText textIdx
+            == ' ' =
+            let (wrappedLine, rest) = Text.splitAt textIdx hardwrappedText
+             in (wrappedLine, Text.tail rest)
+        | otherwise =
+            softWrap hardwrappedText (textIdx - 1)
+
+wordWrap' :: Int -> Text -> [Text]
+wordWrap' maxCol text =
+    Text.splitAt maxCol text
+        |> Tuple.first
+            (\x -> softWrap x <| Text.length x - 1)
+        |> uncurry recurse
+  where
+    softWrap hdwrpdTxt txtIdx
+        | txtIdx <= 0 = (hdwrpdTxt, Text.empty)
+        | Text.index hdwrpdTxt txtIdx == ' ' =
+            Text.splitAt txtIdx hdwrpdTxt
+                |> Tuple.second Text.tail
+        | otherwise =
+            softWrap hdwrpdTxt (txtIdx - 1)
+    recurse (firstLn, ovrflw) nextLn =
+        firstLn
+            : wordWrap' maxCol (ovrflw <> nextLn)
+
+paginates
+    :: ScreenDimensions -> Text -> [Text]
+paginates (ScreenDimensions rows cols) =
+    Text.lines
+        .> concatMap (wordWrap cols)
+        .> groupsOf rows
+        .> map Text.unlines
+
+getTermSize :: IO ScreenDimensions
+getTermSize = case SysInfo.os of
+    unix
+        | unix `elem` ["darwin", "linux"] ->
+            tputScrDim
+    _ -> return $ ScreenDimensions 25 80
+  where
+    tputScrDim :: IO ScreenDimensions
+    tputScrDim = do
+        rows <- getFromTput "lines"
+        cols <- getFromTput "cols"
+        return $ ScreenDimensions rows cols
+      where
+        getFromTput prop =
+            read . init
+                <$> Proc.readProcess "tput" [prop] mempty
 
 run :: IO ()
 run =
